63263de9856e07a246a6baa34ecac7a354d5ea93
vishal <patelvishal2222@yahoo.co.in>
2018-09-12 08:58:47 +0530

    Update database.php

--- php/database.php
+++ php/database.php
@@ -5,6 +5,14 @@ class Database
 	
 	    public $conn;
     function __construct() {
+	    
+	    	   ini_set("display_startup_errors", 1);
+    ini_set("display_errors", 1);
+
+    
+    error_reporting(E_ALL);
+	    
+	    
     session_start();
     $servername = "localhost";
     $dbname = "account";
