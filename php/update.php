75aadc45f4eb567bac3c5ed73fcd922fc91cfdcc
vishal <patelvishal2222@yahoo.co.in>
2018-10-15 08:36:43 +0530

    Delete update.php

--- php/update.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include 'student.php';
-$obj=new Student();
-$data = json_decode(file_get_contents("php://input"));
-$result=$obj->update_student_info($data);
-$message['message']=$result;
-echo json_encode($message);
-?>
