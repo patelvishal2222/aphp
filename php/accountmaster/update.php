89dac5b3273890fccd665a698f29cd1076953a69
vishal <patelvishal2222@yahoo.co.in>
2018-10-15 17:29:43 +0530

    Delete update.php

--- php/accountmaster/update.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include 'accountmaster.php';
-$obj=new AccountMaster();
-$data = json_decode(file_get_contents("php://input"));
-$result=$obj->UpdateRecord($data);
-$message['message']=$result;
-echo json_encode($message);
-?>
